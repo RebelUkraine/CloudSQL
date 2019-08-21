a4d4cfdff0691b9c34ac1d7f070cdeb92ff81bfa
RebelUkraine <support4uf1@gmail.com>
2019-08-21 19:49:51 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "project_name" {
 }
 
 variable "gloud_creds_file" {
-  default = "~/.json/absolute-router-242207-e0142bb87119.json"
+  default = "/root/.json/absolute-router-242207-e0142bb87119.json"
 }
 
 variable "location" {
