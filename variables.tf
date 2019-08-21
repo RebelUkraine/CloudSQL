2d802f23197378e0bc0ffd0e3428400134a1fdcf
Administrator <admin@example.com>
2019-08-21 19:18:52 +0300

    Deploy CloudSQL

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  default = "absolute-router-242207"
+}
+
+variable "gloud_creds_file" {
+  default = "~/.json/absolute-router-242207-e0142bb87119.json"
+}
+
+variable "location" {
+  default = "europe-west1"
+}
+
+// Database configuration
+variable "database_instance_name" {
+  default = "main-postgres-temp-xyz"
+}
+
+resource "random_id" "db_prod_pass" {
+  byte_length = 14
+}
+
+resource "random_id" "db_test_pass" {
+  byte_length = 14
+}
+
